db3baaf4afe207d3d11cebb22b4778a9b8c64f84
Aidan Traboulay <aidantraboulay13@gmail.com>
2026-03-09 19:55:04 -0400

    fix: add r2_key field to ProductImageRecord for proper r2 deletion

--- services/api/internal/media/model.go
+++ services/api/internal/media/model.go
@@ -5,6 +5,7 @@ import "github.com/google/uuid"
 type ProductImageRecord struct {
 	ID        uuid.UUID `json:"id"`
 	ProductID uuid.UUID `json:"product_id"`
+	R2Key     string    `json:"-"`
 	URL       string    `json:"url"`
 	Position  int       `json:"position"`
 }
