772044af7ec9f0337396d3a022d443b262b2d320
Aidan Traboulay <aidantraboulay13@gmail.com>
2026-03-09 15:44:05 -0400

    refactor: remove unused Upload struct from media model

--- services/api/internal/media/model.go
+++ services/api/internal/media/model.go
@@ -2,13 +2,6 @@ package media
 
 import "github.com/google/uuid"
 
-type Upload struct {
-	Key      string    `json:"key"`
-	URL      string    `json:"url"`
-	MimeType string    `json:"mime_type"`
-	Size     int64     `json:"size"`
-}
-
 type ProductImageRecord struct {
 	ID        uuid.UUID `json:"id"`
 	ProductID uuid.UUID `json:"product_id"`
